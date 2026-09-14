95f7f502f0e3a2fa45c1f28a8b4402fa0c1a027c
tjranawat-dev <sltjranawat@gmail.com>
2026-09-14 21:22:15 +0530

    Refresh TejQuote Pro cache on navigation

--- products/tejquote-pro/sw.js
+++ products/tejquote-pro/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tejquote-pro-v1.0.1";
+const CACHE = "tejquote-pro-v1.0.2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -8,9 +8,17 @@ self.addEventListener("activate", event => {
 });
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put("./index.html", copy));
+      return response;
+    }).catch(() => caches.match("./index.html")));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match("./index.html"))));
+  })));
 });
